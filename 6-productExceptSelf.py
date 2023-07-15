0f44878691a05da6cc709ad2c3d863de69a46f51
echokhan <hamzaehsankhan@gmail.com>
2023-07-15 04:03:59 -0700

    own, 2nd unoptimized solution for productExceptSelf

--- 6-productExceptSelf.py
+++ 6-productExceptSelf.py
@@ -19,3 +19,21 @@ class Solution1(object):
           product[i] = reduce(mul, lst)
 
         return list(product.values())
+
+#Own solution
+#Tried to use conditions to find products depending on position
+#Unfortunately, still unoptimized
+#The requirement was that of O(n), and below solution gives O(n2)
+#Failed with time limit exceed after passing 18 / 22 testcases
+class Solution2(object):
+    def productExceptSelf(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        answer = []
+        for i, j in enumerate(nums):
+            if i == 0:  answer.append(reduce(lambda x, y: x*y, (nums[1:])))
+            elif i == len(nums) - 1:  answer.append(reduce(lambda x, y: x*y, nums[:len(nums) - 1]))
+            else:  answer.append(reduce(lambda x, y: x*y, (nums[:i] + nums[i+1:])))
+        return answer
